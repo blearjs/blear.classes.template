f110907f228596d7e16ff43c2addc24d6e9bc7e0
#云淡然 <cloudcome@163.com>
2018-04-18 11:10:47 +0800

    ~ 重构 2.0：build

--- src/build.js
+++ src/build.js
@@ -12,13 +12,17 @@ var array = require('blear.utils.array');
 var object = require('blear.utils.object');
 
 module.exports = function (adapters, args) {
+    var snippet = this;
     var built = {
         code: '',
-        entity: true
+        entity: true,
+        snippet: snippet,
+        open: true,
+        closed: true
     };
 
     array.each(adapters, function (index, adapter) {
-        var ret = adapter.apply(null, args);
+        var ret = adapter.apply(snippet, args);
 
         if (ret) {
             object.assign(built, ret);
